b1d3bac5172612269a008c6eec921ceece4a2ffd
Ninja2402 <94783557+Ninja2402@users.noreply.github.com>
2023-10-26 20:11:05 +0530

    Add files via upload

---
+++ Week6 Peer Graded.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Week6 Peer Graded
+
+# Hello Everyone
+# It is an assignmnet in order to complete course.
+
+# Here are some data science languages
+# 1. Python
+# 2. R
+# 3. MATLAB
+# 4. Scala
+# 5. SQL
+
+# Here are some data science libraries
+# 1. Pandas
+# 2. NumPY
+# 3. SciPy
+# 4. PyTorch
+# 5. TensorFlow
+
+# It is an introduction to an arithmatic operation (addition) between two numbers
+# In addition we add two or more numbers using '+' symbol and print th sum as output
+
+# In[1]:
+
+
+a=10
+b=5
+print(a+b)
+
+
+# In[2]:
+
+
+a=10
+b=20
+c=30
+d=40
+print(a*b+c*d)
+
+
+# In[3]:
+
+
+minutes = float(100)
+hours = float(minutes / 60)
+print(hours)
+
+
+# Here is an unordered list of the objective of this course
+# + Learn about data mining techniques
+# + Learn about data manipulation techniques
+# + Learn about data cleaning techniques
+
+# Author- Aryash Sengar
